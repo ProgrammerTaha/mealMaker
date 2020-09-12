a203c4978ae979192cbb20bc7b18334c6d844522
programmertaha <programmer.taha.1y@gmail.com>
2020-09-12 13:26:26 +0300

    #3#4 Tasks

--- script.js
+++ script.js
@@ -5,6 +5,28 @@ randomly create a three-course meal based on what is available on a menu.
 */
 
 const menu = {
-  _courses: {},//contain a mapping between each course and the dishes available to order in that course.
+  //contain a mapping between each course and the dishes available to order in that course.
+  _courses: {
+    appetizers: [],
+    mains: [],
+    desserts []
+  },
+  get appetizers() {
+    ;
+  }
+  set appetizers(appetizer) {
+    ;
+  }
+  get mains() {
+    ;
+  }
+  set mains(main) {
+    ;
+  }
+  get desserts() {
+    ;
+  }
+  set desserts(dessert) {
+    ;
+  }
 };
-
